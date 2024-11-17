17e02040a06058fa16a9e255a584cf03044e9f26
MHH77 <mesfandiari77@gmail.com>
2024-11-17 08:55:15 -0800

    [feature] Config lazy initialization with @Lazy and globally (we have to just use only in production environment not use both at the same time)

--- src/main/java/com/mhh/ServiceImpl/BaseballCoachImpl.java
+++ src/main/java/com/mhh/ServiceImpl/BaseballCoachImpl.java
@@ -1,12 +1,22 @@
 package com.mhh.ServiceImpl;
 
 import com.mhh.Service.CoachService;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
+@Lazy
 public class BaseballCoachImpl implements CoachService {
+
+    //this component is created at run time even I set it @lazy , because
+    //in controller I need it and (constructor injection) so spring create it at run time and
+    //inject it...but when set lazy globally it not create !!!!
+    public BaseballCoachImpl() {
+        System.out.println("BaseballCoachImpl is created");
+    }
+
     @Override
     public String getWorkout() {
-            return "baseball coach send workout!";
+        return "baseball coach send workout!";
     }
 }
--- src/main/java/com/mhh/ServiceImpl/CoachServiceImpl.java
+++ src/main/java/com/mhh/ServiceImpl/CoachServiceImpl.java
@@ -1,10 +1,18 @@
 package com.mhh.ServiceImpl;
 
 import com.mhh.Service.CoachService;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
+@Lazy
 public class CoachServiceImpl implements CoachService {
+
+    //it's not create in run time
+    public CoachServiceImpl() {
+        System.out.println("CoachServiceImpl is created");
+    }
+
     @Override
     public String getWorkout() {
         return "coach send workout!";
