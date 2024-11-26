dfca869f1f72e261c9712541d4cd058fb7dd8749
MHH77 <mesfandiari77@gmail.com>
2024-11-26 07:06:33 +0330

    [enhancement] Changing project structure

--- src/main/java/com/mhh/Application.java
+++ src/main/java/com/mhh/Application.java
@@ -1,11 +1,8 @@
 package com.mhh;
 
-import com.mhh.Entity.Student;
-import com.mhh.dao.StudentDAO;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -13,90 +10,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
-        return args -> {
-            deleteStudent(studentDAO);
-        };
-    }
-
-    private void createStudent(StudentDAO studentDAO) {
-        // create the student object
-        System.out.println("creating new student object .... ");
-        Student student = new Student("ali", "mohammadi", "A.m@example.com");
-
-        // save the student
-        System.out.println("Saving the student ....");
-        studentDAO.save(student);
-
-        // display id of the saved student
-        System.out.println("Saved student successfully... id is :" + student.getId());
-
-    }
-
-    private void readStudent(StudentDAO studentDAO) {
-        // create the student object
-        System.out.println("creating new student object .... ");
-        Student student = new Student("alimohammad", "mohammadi", "b.m@example.com");
-
-        // save the student
-        System.out.println("Saving the student ...." + student);
-        studentDAO.save(student);
-
-        // display id of the saved student
-        int id = student.getId();
-        System.out.println("Saved student successfully... id is :" + student.getId());
-
-        // retrieve student based on id : primary key
-        System.out.println("Reading the student by id :" + id);
-        Student byId = studentDAO.findById(id);
-
-        // display student
-        System.out.println("Reading student by id successfully... id is :" + byId);
-
-    }
-
-    private void readAllStudent(StudentDAO studentDAO) {
-
-        System.out.println("reading all student objects .... ");
-
-        for (Student student : studentDAO.findAll()) {
-            System.out.println(student);
-        }
-
-    }
-
-    private void fetchStudentByLastname(StudentDAO studentDAO) {
-
-        System.out.println("reading student objects by lastname .... ");
-
-        for (Student student : studentDAO.findByName("mohammadi")) {
-            System.out.println(student);
-        }
-
-    }
-
-    private void updateStudent(StudentDAO studentDAO) {
-        int studentId = 1;
-        System.out.println("updating student object .... ");
-        Student student = studentDAO.findById(studentId);
-        System.out.println("updating student object ... by this id  :" + studentId);
-        student.setFirstName("Ya Ali");
-        System.out.println("updating student object ... " +student);
-        studentDAO.update(student);
-        System.out.println("updating student object successfully... " +student);
-
-    }
-
-    public void deleteStudent(StudentDAO studentDAO) {
-        int studentId = 5;
-        System.out.println("deleting student object .... id is :" + studentId);
-        Student student = studentDAO.findById(studentId);
-        studentDAO.delete(student.getId());
-        System.out.println("deleting student object successfully... id is :" + studentId);
-
-    }
-
-
 }
