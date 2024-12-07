7b9ff93c0bb85befa8bc18a384f3fe3a32b5330c
MHH77 <mesfandiari77@gmail.com>
2024-12-07 06:47:25 +0330

    [feature] Implement global exception handler

---
+++ src/main/java/com/mhh/exceptions/StudentExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.mhh.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class StudentExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setMessage(e.getMessage());
+        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        errorResponse.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception e) {
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setMessage("The parameter entered is not valid.");
+        //errorResponse.setMessage(e.getMessage());
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
--- src/main/java/com/mhh/rest/StudentController.java
+++ src/main/java/com/mhh/rest/StudentController.java
@@ -2,11 +2,8 @@ package com.mhh.rest;
 
 
 import com.mhh.entity.Student;
-import com.mhh.exceptions.StudentErrorResponse;
 import com.mhh.exceptions.StudentNotFoundException;
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -37,33 +34,10 @@ public class StudentController {
 
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
-        if (studentId <= 0 ||  studentId > students.size()) {
+        if (studentId <= 0 || studentId > students.size()) {
             throw new StudentNotFoundException("student id " + studentId + " not found");
         }
         return students.get(studentId);
     }
-
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException e) {
-        StudentErrorResponse errorResponse = new StudentErrorResponse();
-        errorResponse.setMessage(e.getMessage());
-        errorResponse.setStatus(HttpStatus.NOT_FOUND.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException( Exception e) {
-        StudentErrorResponse errorResponse = new StudentErrorResponse();
-        errorResponse.setMessage("The parameter entered is not valid.");
-        //errorResponse.setMessage(e.getMessage());
-        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorResponse.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
-    }
-
 
 }
