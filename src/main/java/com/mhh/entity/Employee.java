a3728ff2f0166eee24a7c7d129d872024a2b259a
MHH77 <mesfandiari77@gmail.com>
2024-12-08 06:47:53 +0330

    [feature] Add new entity - Employee

---
+++ src/main/java/com/mhh/entity/Employee.java
@@ -0,0 +1,24 @@
+package com.mhh.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+}
