f4dd8d7a8beba03ed1675f2e754d78cafd61c7f7
MHH77 <mesfandiari77@gmail.com>
2024-12-16 06:46:42 +0330

    [feature] Add controller Test

---
+++ src/test/java/com/mhh/rest/EmployeeControllerTest.java
@@ -0,0 +1,19 @@
+package com.mhh.rest;
+
+import com.mhh.sevice.EmployeeService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeControllerTest {
+
+    @Mock
+    private EmployeeService employeeService;
+
+    @InjectMocks
+    private EmployeeController employeeController;
+
+
+}
