7ecac9a43113c80d706bd1f2f2bd3afb25f21d10
MHH77 <mesfandiari77@gmail.com>
2024-12-18 07:29:37 +0330

    [feature] Add default page size configuration

--- src/main/java/com/mhh/dao/EmployeeDAO.java
+++ src/main/java/com/mhh/dao/EmployeeDAO.java
@@ -2,9 +2,6 @@ package com.mhh.dao;
 
 import com.mhh.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(path = "members")
 public interface EmployeeDAO extends JpaRepository<Employee, Long> {
 }
