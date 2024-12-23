0d264df8f89a5a76c16f25405d6bdfee0095abe1
MHH77 <mesfandiari77@gmail.com>
2024-12-23 06:40:19 +0330

    [feature] Add letShout service - using Model

--- src/main/java/com/mhh/rest/ThymeleafController.java
+++ src/main/java/com/mhh/rest/ThymeleafController.java
@@ -1,8 +1,10 @@
 package com.mhh.rest;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ThymeleafController {
@@ -22,4 +24,14 @@ public class ThymeleafController {
     public String processForm(Model model) {
         return "say-hello";
     }
+
+    @RequestMapping("/shout")
+    public String letShout(HttpServletRequest request, Model model) {
+
+        String studentName = request.getParameter("studentName");
+        String result = "YO !" + studentName.toUpperCase();
+        model.addAttribute("message", result);
+        return "say-hello";
+    }
+
 }
