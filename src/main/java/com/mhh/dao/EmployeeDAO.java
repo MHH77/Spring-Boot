1cab04129058084d594f14abb73ba70b01191501
MHH77 <mesfandiari77@gmail.com>
2024-12-25 06:37:12 +0330

    [enhancement] Code polishing

--- src/main/java/com/mhh/dao/EmployeeDAO.java
+++
@@ -1,7 +0,0 @@
-package com.mhh.dao;
-
-import com.mhh.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeDAO extends JpaRepository<Employee, Long> {
-}
