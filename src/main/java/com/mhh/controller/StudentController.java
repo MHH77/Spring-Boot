f7ece87bfd49d55206d1250b805363d2219c1706
MHH77 <mesfandiari77@gmail.com>
2024-12-25 06:59:01 +0330

    [feature] Implement login form by thymeleaf and using shortcut for access to data from student-confirmation.html

--- src/main/java/com/mhh/controller/StudentController.java
+++ src/main/java/com/mhh/controller/StudentController.java
@@ -4,13 +4,21 @@ import com.mhh.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
-    @GetMapping("/showForm")
+    @GetMapping("/showStudentForm")
     public String showForm(Model module) {
         Student student = new Student();
         module.addAttribute("student", student);
-        return "student-Form";
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processStudentForm(@ModelAttribute("student") Student student) {
+        System.out.println("the student is " + student.getFirstName() + " / " + student.getLastName());
+        return "student-confirmation";
     }
 }
