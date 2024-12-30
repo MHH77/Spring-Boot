1ba0a56fb6b755130f92ec239e865bedb3bab650
MHH77 <mesfandiari77@gmail.com>
2024-12-30 06:42:47 +0330

    [feature] Implement check box in student-form.html

--- src/main/java/com/mhh/model/Student.java
+++ src/main/java/com/mhh/model/Student.java
@@ -5,6 +5,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -14,5 +16,6 @@ public class Student {
     private String lastName;
     private String country;
     private String favoriteLanguage;
+    private List<String> favoriteSystems;
 
 }
