eb2883842fb9b3ce5586c7a820cc511ad56ece44
MHH77 <mesfandiari77@gmail.com>
2024-12-31 06:47:10 +0330

    [feature] Define new domain - Customer

---
+++ src/main/java/com/mhh/model/Customer.java
@@ -0,0 +1,19 @@
+package com.mhh.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    private String firstName;
+
+    private String lastName;
+
+}
