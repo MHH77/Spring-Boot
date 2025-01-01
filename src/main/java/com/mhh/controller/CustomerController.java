b3411dbb0e3350c37630321b77fa168fa3c56c02
MHH77 <mesfandiari77@gmail.com>
2025-01-01 06:45:46 +0330

    [feature] Implement validation by InitBinder that control white space

--- src/main/java/com/mhh/controller/CustomerController.java
+++ src/main/java/com/mhh/controller/CustomerController.java
@@ -2,15 +2,30 @@ package com.mhh.controller;
 
 import com.mhh.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // add an initbinder ... to convert trim input strings
+    // remove leading and trailing whitespace
+    // resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
 
     @GetMapping("/")
     public String showForm(Model theModel) {
@@ -27,8 +42,7 @@ public class CustomerController {
 
         if (theBindingResult.hasErrors()) {
             return "customer-form";
-        }
-        else {
+        } else {
             return "customer-confirmation";
         }
     }
