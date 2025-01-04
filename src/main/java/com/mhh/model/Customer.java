3f408d75a65d8ff5b01ed1b8c2f4f276afbe47e3
MHH77 <mesfandiari77@gmail.com>
2025-01-04 06:35:46 +0330

    [feature] Implement validation by Max and Min annotation

--- src/main/java/com/mhh/model/Customer.java
+++ src/main/java/com/mhh/model/Customer.java
@@ -1,6 +1,8 @@
 package com.mhh.model;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -19,5 +21,9 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private String lastName = "";
+
+    @Min(value=0, message="must be greater than or equal to zero")
+    @Max(value=10, message="must be less than or equal to 10")
+    private int freePasses;
 
 }
